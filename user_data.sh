f24c73e1fbbe4efcb16ed0130aad9d697eb4958b
Rogers <erogers@amfam.com>
2018-05-03 12:57:17 -0500

    Adding pip install of auto-pep8

--- user_data.sh
+++ user_data.sh
@@ -2,6 +2,7 @@
 
 pip install jupyter_contrib_nbextensions
 pip install jupyter_nbextensions_configurator
+pip install autopep8
 jupyter contrib nbextension install --system
 jupyter nbextensions_configurator enable --system
 
