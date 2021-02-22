ae3c0a88aaa07b46820a324c2eeae5f36b525e02
rmikulec-utilant <59654972+rmikulec-utilant@users.noreply.github.com>
2021-02-22 11:41:31 -0500

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -17,6 +17,10 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get -y update
 apt-get -y install tmux
 
+# Install add-jupyter-kernel script
+cp -p add-jupyter-kernel /usr/local/bin/add-jupyter-kernel
+chown ${NONROOT_USER}: /usr/local/bin/add-jupyter-kernel
+
 unset DEBIAN_FRONTEND
 
 exit 0
